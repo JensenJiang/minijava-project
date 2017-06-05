0edb956ae8fce092ed59e5649bfa8592f65658ee
Jensen Jiang <jensenjiang1995@gmail.com>
2017-06-05 23:24:43 +0800

    hw4 - progressing on LinearScanner.

--- src/spiglet/spiglet2kanga/KangaBuilder.java
+++ src/spiglet/spiglet2kanga/KangaBuilder.java
@@ -1,16 +1,173 @@
 package spiglet.spiglet2kanga;
 
+import spiglet.syntaxtree.*;
 import spiglet.visitor.GJNoArguDepthFirst;
+
+import java.util.Enumeration;
 
 /**
  * Created by jensen on 2017/6/4.
  */
-public class KangaBuilder extends GJNoArguDepthFirst{
-    int global_temp_cnt;
+public class KangaBuilder extends GJNoArguDepthFirst<KangaBuilder.KangaFragment>{
+    int stmt_num;
     int indent;
     int _SPACES_PER_INDENT;
+    LinearScanner _cur_scanner;
+
+    public KangaBuilder() {
+        this.indent = 0;
+        this._SPACES_PER_INDENT = 4;
+    }
+
+    void reset_stmt_num() {
+        this.stmt_num = 0;
+    }
+
+    int allocate_stmt_num() {
+        this.stmt_num ++;
+        return this.stmt_num - 1;
+    }
 
     public class KangaFragment {
+        StringBuilder kanga;
 
+        KangaFragment() {
+            this.kanga = new StringBuilder();
+        }
+
+        KangaFragment append(KangaFragment kf) {
+            this.kanga.append(kf.getString());
+            return this;
+        }
+
+        public String getString() {
+            return this.kanga.toString();
+        }
+
+        KangaFragment append(String s) {
+            this.kanga.append(s);
+            return this;
+        }
+
+        /* kanga writer section starts */
+        void write_indent() {
+            for(int i = 0;i < indent * _SPACES_PER_INDENT;i++) this.append(" ");
+        }
+
+        void write_label(String label) {
+            this.write_indent();
+            this.append(label + "\n");
+        }
+
+        void write_main_begin() {
+            this.write_label("MAIN");
+            inc_indent();
+        }
+
+        void write_main_end() {
+            dec_indent();
+            this.write_label("END");
+        }
+
+        void write_move(int reg_id, String exp) {
+            this.write_indent();
+            this.append("MOVE " + LinearScanner.regs[reg_id] + " " + exp + "\n");
+        }
+
+        void write_astore(int stack_id, int reg_id) {
+            this.write_indent();
+            this.append("ASTORE " + SPILLEDARG(stack_id) + " " + REG(reg_id) + "\n");
+        }
+
+        void write_aload(int reg_id, int stack_id) {
+            this.write_indent();
+            this.append("ALOAD " + REG(reg_id) + " " + SPILLEDARG(stack_id) + "\n");
+        }
+
+        /* kanga writer section ends */
     }
+
+    /* inline */
+    static String SPILLEDARG(int stack_id) {
+        return "SPILLEDARG " + Integer.toString(stack_id);
+    }
+
+    static String REG(int reg_id) {
+        return LinearScanner.regs[reg_id];
+    }
+
+    /* state helper */
+    void inc_indent(){
+        this.indent += 1;
+    }
+    void dec_indent(){
+        this.indent -= 1;
+    }
+
+    /* Visitor Section starts */
+    /**
+     * f0 -> "MAIN"
+     * f1 -> StmtList()
+     * f2 -> "END"
+     * f3 -> ( Procedure() )*
+     * f4 -> <EOF>
+     */
+    public KangaFragment visit(Goal n) {
+        KangaFragment kf = new KangaFragment();
+        this._cur_scanner = new LinearScanner(0);
+
+        n.f0.accept(this);
+        kf.write_main_begin();
+        this.reset_stmt_num();
+
+        n.f1.accept(this._cur_scanner);   // Build flow graph and compute live intervals.
+        n.f1.accept(this);
+
+        n.f2.accept(this);
+        kf.write_main_end();
+        n.f3.accept(this);
+        n.f4.accept(this);
+        return kf;
+    }
+
+    /**
+     * f0 -> ( ( Label() )? Stmt() )*
+     */
+    public KangaFragment visit(StmtList n) {
+        KangaFragment kf;
+        kf = n.f0.accept(this);
+        return kf;
+    }
+
+    public KangaFragment visit(NodeListOptional n) {
+        KangaFragment kf = new KangaFragment();
+        if ( n.present() ) {
+            int _count=0;
+            for (Enumeration<Node> e = n.elements(); e.hasMoreElements(); ) {
+                Node next = e.nextElement();
+                KangaFragment next_kf;
+                if(next instanceof NodeSequence) {
+                    int cur_stmt_num = this.allocate_stmt_num();
+                    for(Node ele : ((NodeSequence) next).nodes) {
+                        if(ele instanceof Label) {
+                            this._cur_scanner.enter_block(cur_stmt_num);
+                            next_kf = ele.accept(this);
+                        }
+                        else if(ele instanceof CJumpStmt || ele instanceof JumpStmt) {
+                            next_kf = ele.accept(this);
+                            this._cur_scanner.enter_block(cur_stmt_num + 1);
+                        }
+                        else {
+                            next_kf = ele.accept(this);
+                        }
+                        kf.append(next_kf);
+                    }
+                }
+
+                _count++;
+            }
+        }
+        return kf;
+    }
+    /* Visitor Section ends */
 }
--- src/spiglet/spiglet2kanga/LinearScanner.java
+++ src/spiglet/spiglet2kanga/LinearScanner.java
@@ -4,153 +4,266 @@ import spiglet.syntaxtree.*;
 import spiglet.visitor.DepthFirstVisitor;
 import sun.jvm.hotspot.utilities.Interval;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Hashtable;
+import java.util.*;
 
 /**
  * Created by jensen on 2017/6/4.
  */
 public class LinearScanner extends DepthFirstVisitor {
     public static String[] regs = {"t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "a0", "a1", "a2", "a3", "v0", "v1"};
-    int stmt_num;
-    Hashtable<Integer, IntervalPair> _temp_cur_interval;
-    ArrayList<IntervalPair> _intervals;
+    public static int MAX_GENERAL_REGS_NUM = 18;
+    BasicBlock _cur_block;      // For both construction stage and running stage
+    Hashtable<Integer, Integer> temp_stack_id;
+    Hashtable<Integer, BasicBlock> entry_stmt_block;
+    int global_stack_id;
 
-    LinearScanner() {
-        this.stmt_num = 0;
+    public LinearScanner(int _stack_start_id) {
+        this.global_stack_id = _stack_start_id;
+        this.temp_stack_id = new Hashtable<>();
+        this.entry_stmt_block = new Hashtable<>();
+        this._cur_block = new BasicBlock(0);
     }
 
-    int allocate_stmt_num() {
-        this.stmt_num ++;
-        return this.stmt_num - 1;
+    int allocate_stack_id() {
+        this.global_stack_id ++;
+        return this.global_stack_id - 1;
     }
 
-    void sort_intervals() {
-        _intervals.sort(IntervalPair.LeftComparator);
-    }
-
-    /* interval helper starts */
-    IntervalPair new_interval(int _L, int _tid) {
-        IntervalPair interval = new IntervalPair(_L, _L, _tid);
-        this._intervals.add(interval);
-        this._temp_cur_interval.put(_tid, interval);
-        return interval;
-    }
-
-    IntervalPair get_interval(int temp_id) {
-        IntervalPair ret = this._temp_cur_interval.get(temp_id);
-        if(ret == null) ret = this.new_interval(0, temp_id);
+    int get_stack_id(int temp_id) {
+        Integer ret = this.temp_stack_id.get(temp_id);
+        if(ret == null) {
+            ret = this.allocate_stack_id();
+            this.temp_stack_id.put(temp_id, ret);
+        }
         return ret;
     }
 
-    void update_interval_R(int temp_id, int _R) {
-        IntervalPair interval = this.get_interval(temp_id);
-        interval.set_second(_R);
+    /* state helper */
+    public void enter_block(int stmt_num) {
+        BasicBlock _to_enter = this.entry_stmt_block.get(stmt_num);
+        assert(_to_enter != null);
+        this._cur_block = _to_enter;
     }
 
-    void update_interval_L(int temp_id, int _L) {
-        IntervalPair interval = this.new_interval(_L, temp_id);
-    }
+
+    class BasicBlock extends DepthFirstVisitor {
+        int stmt_num;
+        Hashtable<Integer, IntervalPair> _temp_cur_interval;
+        Hashtable<Integer, Integer> _temp2reg;
+        ArrayList<IntervalPair> _intervals;
+        ArrayList<Integer> _avail_regs;
+        PriorityQueue<IntervalPair> active_list;
+
+        BasicBlock(int _start) {
+            this.stmt_num = _start;
+            entry_stmt_block.put(this.stmt_num, this);
+            this._temp_cur_interval = new Hashtable<>();
+            this._intervals = new ArrayList<>();
+        }
+
+        int allocate_stmt_num() {
+            this.stmt_num ++;
+            return this.stmt_num - 1;
+        }
+
+        public void running_stage_init() {
+            this._intervals.sort(IntervalPair.LeftComparator);
+            this.active_list = new PriorityQueue<>(IntervalPair.RightComparator);
+            this._avail_regs = new ArrayList<>();
+            for(int i = 0;i < MAX_GENERAL_REGS_NUM;i++) this._avail_regs.add(i);
+        }
+
+        void remove_outdated(int cur_stmt_num, KangaBuilder.KangaFragment kf) { // pass the maximum stmt_num + 1 to this method, it would clean out all the regsiters
+            ArrayList<IntervalPair> to_remove = new ArrayList<>();
+            for(IntervalPair e : active_list) {
+                if(e.get_second() < cur_stmt_num) {
+                    to_remove.add(e);
+                    /* recycle the register */
+                    Integer reg_id = this._temp2reg.remove(e.temp_id);
+                    assert(reg_id != null);
+                    kf.write_astore(get_stack_id(e.temp_id), reg_id);
+                    _avail_regs.add(reg_id);
+                }
+            }
+            for(IntervalPair e : to_remove) {
+                active_list.remove(e);
+            }
+        }
+
+        public void register_allocation(int cur_stmt_num, KangaBuilder.KangaFragment kf) {
+            boolean has_clean = false;
+            while(!this._intervals.isEmpty() && this._intervals.get(0).get_first() <= cur_stmt_num) {
+                if(!has_clean) {
+                    this.remove_outdated(cur_stmt_num, kf);
+                    has_clean = true;
+                }
+                IntervalPair cur_interval = this._intervals.remove(0);
+                /* exists available register */
+                if(!this._avail_regs.isEmpty()) {
+                    int reg_id = this._avail_regs.remove(0);
+                    _temp2reg.put(cur_interval.temp_id, reg_id);
+                    kf.write_aload(reg_id, get_stack_id(cur_interval.temp_id));
+                }
+                else {
+                    // TODO: when there is no available register!
+                    hahaha
+                }
+            }
+        }
+
+        void sort_intervals() {
+            _intervals.sort(IntervalPair.LeftComparator);
+        }
+
+        /* interval helper starts */
+        IntervalPair new_interval(int _L, int _tid) {
+            IntervalPair interval = new IntervalPair(_L, _L, _tid);
+            this._intervals.add(interval);
+            this._temp_cur_interval.put(_tid, interval);
+            return interval;
+        }
+
+        IntervalPair get_interval(int temp_id) {
+            IntervalPair ret = this._temp_cur_interval.get(temp_id);
+            if(ret == null) ret = this.new_interval(0, temp_id);
+            return ret;
+        }
+
+        void update_interval_R(int temp_id, int _R) {
+            IntervalPair interval = this.get_interval(temp_id);
+            interval.set_second(_R);
+        }
+
+        void update_interval_L(int temp_id, int _L) {
+            IntervalPair interval = this.new_interval(_L, temp_id);
+        }
 
     /* interval helper ends */
 
 
     /* Visitor Section starts */
-    /**
-     * f0 -> "NOOP"
-     */
-    public void visit(NoOpStmt n) {
-        this.allocate_stmt_num();
-        n.f0.accept(this);
+        /**
+         * f0 -> "NOOP"
+         */
+        public void visit(NoOpStmt n) {
+            this.allocate_stmt_num();
+            n.f0.accept(this);
+        }
+
+        /**
+         * f0 -> "ERROR"
+         */
+        public void visit(ErrorStmt n) {
+            this.allocate_stmt_num();
+            n.f0.accept(this);
+        }
+
+        /**
+         * f0 -> "CJUMP"
+         * f1 -> Temp()
+         * f2 -> Label()
+         */
+        public void visit(CJumpStmt n) {
+            int cur_stmt_num = this.allocate_stmt_num();
+            n.f0.accept(this);
+            n.f1.accept(this);
+            this.update_interval_R(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
+            n.f2.accept(this);
+        }
+
+        /**
+         * f0 -> "JUMP"
+         * f1 -> Label()
+         */
+        public void visit(JumpStmt n) {
+            int cur_stmt_num = this.allocate_stmt_num();
+            n.f0.accept(this);
+            n.f1.accept(this);
+        }
+
+        /**
+         * f0 -> "HSTORE"
+         * f1 -> Temp()
+         * f2 -> IntegerLiteral()
+         * f3 -> Temp()
+         */
+        public void visit(HStoreStmt n) {
+            int cur_stmt_num = this.allocate_stmt_num();
+            n.f0.accept(this);
+            n.f1.accept(this);
+            this.update_interval_R(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
+            n.f2.accept(this);
+            n.f3.accept(this);
+            this.update_interval_R(Integer.parseInt(n.f3.f1.f0.tokenImage), cur_stmt_num);
+        }
+
+        /**
+         * f0 -> "HLOAD"
+         * f1 -> Temp()
+         * f2 -> Temp()
+         * f3 -> IntegerLiteral()
+         */
+        public void visit(HLoadStmt n) {
+            int cur_stmt_num = this.allocate_stmt_num();
+            n.f0.accept(this);
+            n.f1.accept(this);
+            this.update_interval_L(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
+            n.f2.accept(this);
+            this.update_interval_R(Integer.parseInt(n.f2.f1.f0.tokenImage), cur_stmt_num);
+            n.f3.accept(this);
+        }
+
+        /**
+         * f0 -> "MOVE"
+         * f1 -> Temp()
+         * f2 -> Exp()
+         */
+        public void visit(MoveStmt n) {
+            int cur_stmt_num = this.allocate_stmt_num();
+            n.f0.accept(this);
+            n.f1.accept(this);
+            this.update_interval_L(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
+            n.f2.accept(this);
+        }
+
+        /**
+         * f0 -> "PRINT"
+         * f1 -> SimpleExp()
+         */
+        public void visit(PrintStmt n) {
+            int cur_stmt_num = this.allocate_stmt_num();
+            n.f0.accept(this);
+            n.f1.accept(this);
+            if(n.f1.f0.choice instanceof Temp) this.update_interval_R(Integer.parseInt(((Temp)n.f1.f0.choice).f1.f0.tokenImage), cur_stmt_num);
+        }
     }
 
-    /**
-     * f0 -> "ERROR"
-     */
-    public void visit(ErrorStmt n) {
-        this.allocate_stmt_num();
-        n.f0.accept(this);
+    /* Visitor Section starts */
+    public void visit(NodeListOptional n) {
+        if ( n.present() )
+            for (Enumeration<Node> e = n.elements(); e.hasMoreElements(); ) {
+                Node next = e.nextElement();
+                if(next instanceof NodeSequence) {
+                    for(Node ele : ((NodeSequence)next).nodes) {
+                        if(ele instanceof Label) {
+                            BasicBlock _new_block = new BasicBlock(this._cur_block.stmt_num);
+                            this._cur_block = _new_block;
+                            ele.accept(this._cur_block);
+                        }
+                        else if(ele instanceof CJumpStmt || ele instanceof JumpStmt) {
+                            ele.accept(this._cur_block);
+                            BasicBlock _new_block = new BasicBlock(this._cur_block.stmt_num);
+                            this._cur_block = _new_block;
+                        }
+                        else {
+                            ele.accept(this._cur_block);
+                        }
+                    }
+                }
+            }
     }
+    /* Visitor Section ends */
 
-    /**
-     * f0 -> "CJUMP"
-     * f1 -> Temp()
-     * f2 -> Label()
-     */
-    public void visit(CJumpStmt n) {
-        int cur_stmt_num = this.allocate_stmt_num();
-        n.f0.accept(this);
-        n.f1.accept(this);
-        this.update_interval_R(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
-        n.f2.accept(this);
-    }
-
-    /**
-     * f0 -> "JUMP"
-     * f1 -> Label()
-     */
-    public void visit(JumpStmt n) {
-        int cur_stmt_num = this.allocate_stmt_num();
-        n.f0.accept(this);
-        n.f1.accept(this);
-    }
-
-    /**
-     * f0 -> "HSTORE"
-     * f1 -> Temp()
-     * f2 -> IntegerLiteral()
-     * f3 -> Temp()
-     */
-    public void visit(HStoreStmt n) {
-        int cur_stmt_num = this.allocate_stmt_num();
-        n.f0.accept(this);
-        n.f1.accept(this);
-        this.update_interval_R(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
-        n.f2.accept(this);
-        n.f3.accept(this);
-        this.update_interval_R(Integer.parseInt(n.f3.f1.f0.tokenImage), cur_stmt_num);
-    }
-
-    /**
-     * f0 -> "HLOAD"
-     * f1 -> Temp()
-     * f2 -> Temp()
-     * f3 -> IntegerLiteral()
-     */
-    public void visit(HLoadStmt n) {
-        int cur_stmt_num = this.allocate_stmt_num();
-        n.f0.accept(this);
-        n.f1.accept(this);
-        this.update_interval_L(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
-        n.f2.accept(this);
-        this.update_interval_R(Integer.parseInt(n.f2.f1.f0.tokenImage), cur_stmt_num);
-        n.f3.accept(this);
-    }
-
-    /**
-     * f0 -> "MOVE"
-     * f1 -> Temp()
-     * f2 -> Exp()
-     */
-    public void visit(MoveStmt n) {
-        int cur_stmt_num = this.allocate_stmt_num();
-        n.f0.accept(this);
-        n.f1.accept(this);
-        this.update_interval_L(Integer.parseInt(n.f1.f1.f0.tokenImage), cur_stmt_num);
-        n.f2.accept(this);
-    }
-
-    /**
-     * f0 -> "PRINT"
-     * f1 -> SimpleExp()
-     */
-    public void visit(PrintStmt n) {
-        int cur_stmt_num = this.allocate_stmt_num();
-        n.f0.accept(this);
-        n.f1.accept(this);
-        if(n.f1.f0.choice instanceof Temp) this.update_interval_R(Integer.parseInt(((Temp)n.f1.f0.choice).f1.f0.tokenImage), cur_stmt_num);
-    }
 }
 
 class Pair<A, B> {
@@ -186,9 +299,9 @@ class IntervalPair extends Pair<Integer, Integer> {
         }
     };
 
-    public static Comparator<IntervalPair> RightComparator = new Comparator<IntervalPair>() {
+    public static Comparator<IntervalPair> RightComparator = new Comparator<IntervalPair>() {   // descending order
         public int compare(IntervalPair a, IntervalPair b) {
-            return a.second.compareTo(b.second);
+            return b.second.compareTo(a.second);
         }
     };
 }
